a07f45f9c9ac23bb8e3998d23c82e9c0be563b50
Patrycja <95099147+patrgor91@users.noreply.github.com>
2023-01-18 20:47:09 +0100

    chore: make hibernate with Place entity working. (#1)
    Co-authored-by: Pawel@ThinkPad <pawel@piekut.org>

--- src/main/java/com/example/thepathproject2/entities/Place.java
+++ src/main/java/com/example/thepathproject2/entities/Place.java
@@ -1,25 +1,58 @@
 package com.example.thepathproject2.entities;
 
-import com.example.thepathproject2.models.Comment;
-import com.example.thepathproject2.models.Note;
-import com.example.thepathproject2.models.PlaceType;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.List;
-
-
-    @Data
-    @NoArgsConstructor
     @Entity
+    @Table(name = "PLACE")
     public class Place {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Integer id;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getZipcode() {
+            return zipcode;
+        }
+
+        public void setZipcode(String zipcode) {
+            this.zipcode = zipcode;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
 
         private String name;
         private String zipcode;
--- src/main/java/com/example/thepathproject2/models/Comment.java
+++ src/main/java/com/example/thepathproject2/models/Comment.java
@@ -1,15 +1,15 @@
-package com.example.thepathproject2.models;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-
-public class Comment {
-
-    private String comment;
-
-}
+//package com.example.thepathproject2.models;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//
+//public class Comment {
+//
+//    private String comment;
+//
+//}
